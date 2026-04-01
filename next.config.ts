551baa893c54347c2727a70a630bd5398a7f4d04
Berenguus <jobeal2006@gmail.com>
2026-04-01 11:34:29 +0200

    Fix next config structure for Next 16

--- next.config.ts
+++ next.config.ts
@@ -11,10 +11,7 @@ const nextConfig: NextConfig = {
   },
   typescript: {
     ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  }
 };
 
 export default nextConfig;
